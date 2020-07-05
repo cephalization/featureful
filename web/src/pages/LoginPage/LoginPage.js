f8ba54b73680204ef133b2cac9dfcf403687d0fb
Anthony Powell <anthonypowell@hey.com>
2020-07-05 12:01:18 -0400

    Remove unnecessary routing logic

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,13 +1,8 @@
-import { navigate, routes, Redirect } from '@redwoodjs/router'
+import { routes, Redirect } from '@redwoodjs/router'
 import { useAuth } from '@redwoodjs/auth'
 
 const LoginPage = () => {
-  const { logIn, isAuthenticated } = useAuth()
-
-  const handleLogin = () => {
-    logIn()
-    navigate(routes.dashboard())
-  }
+  const { isAuthenticated, logIn } = useAuth()
 
   if (isAuthenticated) {
     return <Redirect to={routes.dashboard()} />
@@ -40,7 +35,7 @@ const LoginPage = () => {
                 <span className="w-full inline-flex rounded-md shadow-sm">
                   <button
                     type="button"
-                    onClick={handleLogin}
+                    onClick={logIn}
                     className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
                   >
                     Login
